6508ab1cf67cc43ac176badaeb0e9c293af6dba0
Andrey Serebrov <abserebrov@avito.ru>
2021-02-04 21:16:24 +0300

    тест

--- script.js
+++ script.js
@@ -3,33 +3,29 @@ let profileTitle = document.querySelector(".profile__title");
 let profileSubtitle = document.querySelector(".profile__subtitle");
 let formName = document.querySelector(".form__name");
 let formBio = document.querySelector(".form__bio");
+let editButton = document.querySelector('.profile__edit-button');
+let saveButton = document.querySelector('.form__save');
+let closeButton = document.querySelector('.form__cross');
 
-profileTitle.textContent = "Ttcn"
 
-document.querySelector('.profile__edit-button').addEventListener('click', function (e) {
-
+function openPopup(){
   formName.value = profileTitle.textContent;
   formBio.value = profileSubtitle.textContent;
 
-  console.log(profileTitle.textContent)
+   popup.classList.add("popup_opened");
+}
 
-  popup.classList.add("popup_opened");
-});
-
-
-document.querySelector('.form__save').addEventListener('click', function (e) {
+function save(){
   
+  profileTitle.textContent =  formName.value ;
+  profileSubtitle.textContent = formBio.value;
+  closePopup();
+}
 
-
-  // profileTitle.textContent =  formName.value ;
-  // profileSubtitle.textContent = formBio.value;
-
-  // console.log('Хуйажщлжлыфждвлфждывлжфылвждфылвждыфлвжлыфжвдлфыжвлыфжвлждлы')
-
-  // popup.classList.remove("popup_opened");
-});
-
-
-document.querySelector('.form__cross').addEventListener('click', function (e) {
+function closePopup (){
   popup.classList.remove("popup_opened");
-});
+}
+
+editButton.addEventListener('click', openPopup);
+saveButton.addEventListener('click', save);
+closeButton.addEventListener('click', closePopup);

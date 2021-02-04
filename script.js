bccab9ec674337c81128cce99b8dd996348553f1
Andrey Serebrov <abserebrov@avito.ru>
2021-02-04 20:20:41 +0300

    тест

--- script.js
+++ script.js
@@ -1,30 +1,35 @@
+let popup = document.querySelector('.popup');
+let profileTitle = document.querySelector(".profile__title");
+let profileSubtitle = document.querySelector(".profile__subtitle");
+let formName = document.querySelector(".form__name");
+let formBio = document.querySelector(".form__bio");
+
+profileTitle.textContent = "Ttcn"
+
 document.querySelector('.profile__edit-button').addEventListener('click', function (e) {
-  let popup = document.querySelector('.popup');
-  let profileTitle = document.querySelector(".profile__title");
-  let profileSubtitle = document.querySelector(".profile__subtitle");
-  
+
+  formName.value = profileTitle.textContent;
+  formBio.value = profileSubtitle.textContent;
 
   console.log(profileTitle.textContent)
-  document.querySelector(".form__name").value = profileTitle.textContent;
-  document.querySelector(".form__bio").value = profileSubtitle.textContent;
 
   popup.classList.add("popup_opened");
 });
 
 
 document.querySelector('.form__save').addEventListener('click', function (e) {
-  let popup = document.querySelector('.popup');
-  let profileTitle = document.querySelector(".profile__title");
-  let profileSubtitle = document.querySelector(".profile__subtitle");
   
-  profileTitle.textContent = document.querySelector(".form__name").value;
-  profileSubtitle.textContent = document.querySelector(".form__bio").value;
 
-  popup.classList.remove("popup_opened");
+
+  // profileTitle.textContent =  formName.value ;
+  // profileSubtitle.textContent = formBio.value;
+
+  // console.log('Хуйажщлжлыфждвлфждывлжфылвждфылвждыфлвжлыфжвдлфыжвлыфжвлждлы')
+
+  // popup.classList.remove("popup_opened");
 });
 
 
 document.querySelector('.form__cross').addEventListener('click', function (e) {
-  let popup = document.querySelector('.popup');
   popup.classList.remove("popup_opened");
 });
